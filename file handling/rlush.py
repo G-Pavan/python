66eac2e315b7155646db2d97b95b7a4937bf9314
G-Pavan <pa156v@gmail.com>
2025-07-29 16:03:06 +0530

    Add files via upload

---
+++ file handling/rlush.py
@@ -0,0 +1,12 @@
+filename='root.txt'
+with open(filename,'w') as f:
+    f.write("hii,vdu..\n")
+with open(filename,'r+') as file:
+    print("latest data:")
+    print(file.read())
+    file.seek(1)
+    userinput=input("\n enter text to overwrite from begining ")
+    file.write(userinput)
+    file.seek(1)
+    print("\n file after writing by r+")
+    print(file.read())
